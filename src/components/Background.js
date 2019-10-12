1f434747e9d5f9467eccc0832fe4265da5d04874
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-10-12 01:18:39 -0400

    background img setup

---
+++ src/components/Background.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Background.css";
+
+
+function Background() {
+  return (
+    <img className="bg" alt="maple" src={require('../images/maple-wallpaper.jpg')}></img>
+
+  );
+}
+
+export default Background;
